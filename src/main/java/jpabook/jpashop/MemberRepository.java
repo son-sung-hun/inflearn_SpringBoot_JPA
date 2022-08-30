4ab286a05ed24ec58c8823192cdf9c6d1271aa71
son-sung-hun <tjdgns8482@gmail.com>
2022-08-30 17:11:05 +0900

    application.yml 생성

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){ //생성
+       em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){ //조회
+        return em.find(Member.class, id);
+    }
+}
