619981a073488a9065b5c059e899ca5809a3c97f
son-sung-hun <tjdgns8482@gmail.com>
2022-09-07 23:33:29 +0900

    상품 엔티티 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //비즈니스 로직
+    //재고 증가 로직
+    public void addStock(int stockQuantity){
+        this.stockQuantity += stockQuantity;
+    }
+
+    //감소
+
+    public void removeStock(int stockQuantity){
+        int stock = this.stockQuantity - stockQuantity;
+        if(stock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = stock;
+    }
 }
